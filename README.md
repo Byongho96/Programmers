775998ae0cec6b31c0699249b00ef890912b563b
Byongho <unlike96@gmail.com>
2022-09-02 18:25:38 +0900

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+Solutions for the problems in Programmers website
+
+This repository is created on 09.02.22
---
+++ codes/성격_유형_검사하기.py
@@ -0,0 +1,10 @@
+def solution(survey, choices):
+
+    N = len(survey)
+    types = [0] * 8 #
+
+    for i in range(N):
+
+    for 
+    answer = ''
+    return answer
